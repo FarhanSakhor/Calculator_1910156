b7ab72437842e9c6adfbb423dcc96a5644a3f8b0
FeRN <fasakhor@gmail.com>
2024-11-09 14:44:05 +0600

    Done building the prototype

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -7,16 +7,16 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView resultText, solutionText;
     Button but1, but2, but3, but4, but5, but6, but7, but8, but9, but0, butDot;
     Button butPlus, butMinus, butMultiply, butDivision, butEqual;
-    Button butC, butD;
+    Button butAC, butDel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,17 +42,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         assignId(butMultiply, R.id.butMultiply);
         assignId(butDivision, R.id.butDivision);
         assignId(butEqual, R.id.butEqual);
-        assignId(butC, R.id.butC);
-        assignId(butD, R.id.butD);
+        assignId(butAC, R.id.butAC);
+        assignId(butDel, R.id.butDel);
     }
 
     @Override
     public void onClick(View v) {
+        Button button = (Button) v;
+        String buttonText= button.getText().toString();
+        String dataToCalculate = solutionText.getText().toString();
 
+        if(buttonText.equals("AC"))
+        {
+            solutionText.setText("");
+            resultText.setText("0");
+            return;
+        }
+        if(buttonText.equals("="))
+        {
+            solutionText.setText(resultText.getText());
+            return;
+        }
+        if(buttonText.equals("DEL"))
+        {
+            dataToCalculate= dataToCalculate.substring(0,dataToCalculate.length()-1);
+        }
+        else {
+            dataToCalculate= dataToCalculate+buttonText;
+        }
+
+        solutionText.setText(dataToCalculate);
+
+        String result= getResult(dataToCalculate);
+
+        if(!result.equals("Error"))
+        {
+            resultText.setText(result);
+        }
     }
     void assignId(Button but, int id)
     {
         but= findViewById(id);
         but.setOnClickListener(this);
     }
+
+    String getResult(String data)
+    {
+        try {
+            Context context= Context.enter();
+            context.setOptimizationLevel(-1);
+            Scriptable scriptable= context.initSafeStandardObjects();
+            String result= context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
+            if (result.endsWith(".0"))
+            {
+                result=result.replace(".0", "");
+            }
+            return result;
+        }catch (Exception e)
+        {
+            return "Error";
+        }
+    }
+
 }
